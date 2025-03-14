7669392364a502a194b61aead194e32b5e2b32d2
FrancissC05 <dev.fcartagena@gmail.com>
2025-03-14 15:24:13 -0500

    Implement post method and fix get response structure

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,12 +4,33 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET() {
     try {
         const users = await prisma.user.findMany({
+            where: {assignedToId: null},
             include: {
                 assignedTo: true,
             }
         });
         
         return NextResponse.json({users});
+    } catch (error: any) {
+        return NextResponse.json(
+            {error: error.message}, 
+            {status: 500}
+        );
+    }
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const {name,email,assignedToId} = await request.json();
+        const users = await prisma.user.create({
+            data: {
+                name,
+                email,
+                assignedToId: assignedToId || null
+            }
+        });
+           
+        return NextResponse.json({message: "User created succesfully",users});
     } catch (error: any) {
         return NextResponse.json(
             {error: error.message}, 
