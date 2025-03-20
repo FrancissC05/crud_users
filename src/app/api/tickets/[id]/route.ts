a36e8a1b607b851674df2db5b7c84f409792341a
FrancissC05 <dev.fcartagena@gmail.com>
2025-03-20 00:50:27 -0500

    implement put endpoint for ticket update

--- src/app/api/tickets/[id]/route.ts
+++ src/app/api/tickets/[id]/route.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 interface Params {
     params: Promise<{id: string}>;
@@ -32,6 +32,42 @@ export async function GET(_:any, {params}: Params) {
         }
 
         return NextResponse.json(ticket);
+
+    } catch (error: any) {
+        return NextResponse.json(
+            {error: error.message}, 
+            {status: 500}
+        );
+    }
+}
+
+export async function PUT(request: NextRequest, {params}: Params) {
+    try {
+        const {id} = await params;
+        const body = await request.json();
+
+        const existingTicket = await prisma.ticket.findUnique({
+            where: {id}
+        });
+
+        if (!existingTicket) {
+            return NextResponse.json({ error: "Ticket not found" }, { status: 404 });
+        }
+
+        const ticket = await prisma.ticket.update({
+            data: {
+                title: body.title,
+                description: body.description,
+                status: body.status,
+                assignedTo: {
+                    connect: {id: body.assignedTo}
+                }
+            }, 
+            where: {id}
+            
+        });
+
+        return NextResponse.json({message: "Ticket updated succesfully",ticket});
 
     } catch (error: any) {
         return NextResponse.json(
